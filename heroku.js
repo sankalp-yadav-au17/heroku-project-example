7ff308e0ef27673329065f120c79b5b8d4bc8a8b
sankalp-yadav-au17 <sankalp_ma@srmuniv.edu.in>
2021-10-02 15:36:33 +0530

    made changes in the port

--- heroku.js
+++ heroku.js
@@ -5,7 +5,8 @@ app.get('/',(req,res)=>{
     res.send(`Server is working. yo`)
 })
 
-const PORT = 3000
+console.log(`heroku port is ${process.env.PORT}`);
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT,()=>{
     console.log(`Server Started at ${PORT} port`);
