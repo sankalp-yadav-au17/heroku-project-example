ea9c0f61581a7f94e99b43d30a843348a17648f4
sankalp-yadav-au17 <sankalp_ma@srmuniv.edu.in>
2021-10-03 07:10:20 +0530

    made changes

--- queue/queue_heroku1.js
+++ queue/queue_heroku1.js
@@ -6,16 +6,14 @@ app.use(express.json())
 const {MongoClient, ObjectId} = require('mongodb')
 const db_url = `mongodb+srv://queue_app:NnlgQuYNSTOEbv1f@cluster0.zu8fe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const client = new MongoClient(db_url)
-let dbRef = null
-// NnlgQuYNSTOEbv1f
-// queue_app
+
 async function init(){
     await client.connect()
     db = client.db('queue_app')
 }
 init()
 
-app.post("/queue",async(req,res)=>{
+app.post("/",async(req,res)=>{
     console.log("comming from insomnia");
     console.log(req.body)
     const collection_ref = db.collection('details')
@@ -23,20 +21,25 @@ app.post("/queue",async(req,res)=>{
     res.json(insert_result)
 })
 
-app.get("/queue",async(req,res)=>{
+app.get("/",async(req,res)=>{
     const collection_ref = db.collection('details')
     const all_products = await collection_ref.find({}).toArray()
     res.json(all_products)
 })
 
-collection_ref.
-app.delete("/queue/:uniqueId",async(req,res)=>{
+app.get("/:uniqueId",async(req,res)=>{
+    const collection_ref = db.collection('details')
+    const one_product = await collection_ref.findOne({_id: new ObjectId(req.params.uniqueId)})
+    res.json(one_product)
+})
+
+app.delete("/:uniqueId",async(req,res)=>{
     const collection_ref = db.collection('details')
     const one_product = await collection_ref.deleteOne({"_id": new ObjectId(req.params.uniqueId)})
     res.json(one_product)
 })
 
-app.put("/queue/:uniqueId",async(req,res)=>{
+app.put("/:uniqueId",async(req,res)=>{
     const collection_ref = db.collection('details')
     const updating = await collection_ref.updateOne({_id: new ObjectId(req.params.uniqueId)},{$set:req.body})
      res.json(updating)
@@ -50,4 +53,4 @@ app.listen(PORT,()=>{
 })
 
 
-// https://heroku-exaple-app.herokuapp.com/
+// https://heroku-exaple-app.herokuapp.com/queue
