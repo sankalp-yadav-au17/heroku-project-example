324f7b599caf6d11107a1a9a364726606b45be47
sankalp-yadav-au17 <sankalp_ma@srmuniv.edu.in>
2021-10-03 07:42:10 +0530

    checking only html file

--- queue/queue_heroku1.js
+++ queue/queue_heroku1.js
@@ -7,31 +7,16 @@ const {MongoClient, ObjectId} = require('mongodb')
 const db_url = `mongodb+srv://queue_app:NnlgQuYNSTOEbv1f@cluster0.zu8fe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const client = new MongoClient(db_url)
 
-async function init(){
-    await client.connect()
-    db = client.db('queue_app')
-}
-init()
+// async function init(){
+//     await client.connect()
+//     db = client.db('queue_app')
+// }
+// init()
 
 app.get("/queue",async(req,res)=>{
     console.log("reached here");
     res.sendFile(`${__dirname}/queue_heroku.html`)
 })
-
-app.post("/queue/",async(req,res)=>{
-    console.log("comming from insomnia");
-    console.log(req.body)
-    const collection_ref = db.collection('details')
-    const insert_result = await collection_ref.insertOne(req.body)
-    res.json(insert_result)
-})
-
-app.get("/queue/",async(req,res)=>{
-    const collection_ref = db.collection('details')
-    const all_products = await collection_ref.find({}).toArray()
-    res.json(all_products)
-})
-
 
 
 console.log(`heroku port is ${process.env.PORT}`);
