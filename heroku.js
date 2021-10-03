5ea777f5d9bed0e7412e384b5b289f4b4c6d92ab
sankalp-yadav-au17 <sankalp_ma@srmuniv.edu.in>
2021-10-03 21:39:30 +0530

    a

--- heroku.js
+++ heroku.js
@@ -25,7 +25,7 @@ app.post("/",async(req,res)=>{
 app.get("/",async(req,res)=>{
     const collection_ref = db.collection('book_details')
     const all_products = await collection_ref.find({}).toArray()
-    res.json(all_products)
+    // res.json(all_products)
     res.json(req.body)
 })
 
